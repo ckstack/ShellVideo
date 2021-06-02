ec915f5f9961ef101b0d4a5a7f8373951aac0937
ckstack <1210228640@qq.com>
2021-06-02 13:01:39 +0800

    应用添加自定义Application

---
+++ app/src/main/java/com/mixstack/shellvideo/VApplication.java
@@ -0,0 +1,10 @@
+package com.mixstack.shellvideo;
+
+import android.app.Application;
+
+public class VApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
